a7946824dcef1115d0dc243f8aedacba05c16fe0
Butterscotch! <bscotchvanilla@gmail.com>
2022-06-02 00:12:21 -0400

    Use the ExternalHR SDK

---
+++ FitbitWebOSC/HRtoVRChat/HRSDKInstance.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics;
+using HRtoVRChat_OSC_SDK;
+
+namespace FitbitWebOSC.HRtoVRChat
+{
+    public class HRSDKInstance : ExternalHRSDK, IDisposable
+    {
+        /// <summary>
+        /// The name of your SDK
+        /// </summary>
+        public override string SDKName { get => "fitbitweb"; set => throw new InvalidOperationException("The SDK name can not be modified"); }
+
+        /// <summary>
+        /// The current HeartRate
+        /// </summary>
+        public override int HR { get; set; } = 80;
+
+        /// <summary>
+        /// If the device transmitting data to the source is connected.
+        /// If your service does not support this, then you can point it to IsActive
+        /// </summary>
+        public override bool IsOpen { get; set; } = true;
+
+        /// <summary>
+        /// If there's an active connection to the source
+        /// </summary>
+        public override bool IsActive { get; set; } = true;
+
+        public Stopwatch Timer = new();
+        public TimeSpan Interval = new();
+
+        public override bool Initialize()
+        {
+            // TODO Actually return whether this should be used
+            Console.WriteLine($"Started the FitBit Web API extension for HRtoVRChat_OSC");
+            return true;
+        }
+
+        public override void Update()
+        {
+            if (!Timer.IsRunning || Timer.Elapsed > Interval)
+            {
+                // Start/Restart the timer
+                Timer.Restart();
+
+
+            }
+        }
+
+        public override void Destroy()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            // TODO Close the web connection (if possible?)
+        }
+    }
+}
